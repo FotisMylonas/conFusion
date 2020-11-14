1c592406d6c84a235b5e514edbe12e2b3b8be8d6
fotismylonas <fmylontech@hotmail.com>
2020-11-14 18:09:12 +0200

    Peer-graded Assignment: Angular Services, Routing and SPA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { PromotionService } from './services/promotion.service';
 import { DishService } from './services/dish.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,7 @@ import { DishService } from './services/dish.service';
     MatButtonModule,
     AppRoutingModule,
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
